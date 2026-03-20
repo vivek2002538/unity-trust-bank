a94d7b8189ae1c33b3e5da4a42d9dfd6a2ee1525
vivek2002538 <gaadhamshettivivek3@gmail.com>
2026-03-20 18:38:00 +0530

    fixed django version

--- bank/settings.py
+++ bank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-()$9c#zfrajifg8#@sizjh#*79e)hio0qdsxo%rf2yx2kyr(x-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
